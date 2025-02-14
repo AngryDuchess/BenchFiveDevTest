8e4cd97fd4fdd3cd8ee80e3ca26b05f9288352b6
Mahama Hamida <mahamahamida@gmail.com>
2025-02-14 14:39:45 +0000

    refactor ProductCard component for improved code readability and consistent formatting

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface ProductCardProps {
   image: string;
@@ -6,24 +6,33 @@ interface ProductCardProps {
   description: string;
   price: string;
   sku: string;
-  productType: 'DVD' | 'Book' | 'Furniture';
+  productType: "DVD" | "Book" | "Furniture";
   productSpecificValue: string;
   isSelected: boolean;
   onSelect: () => void;
 }
 
-const productTypes =  (str: string
-) => ({
-    DVD: `Size: ${str} MB`,
-    Book: `Weight: ${str} Kg`,
-    Furniture: `Dimensions: ${str}`
-  })
+const productTypes = (str: string) => ({
+  DVD: `Size: ${str} MB`,
+  Book: `Weight: ${str} Kg`,
+  Furniture: `Dimensions: ${str}`,
+});
 
-const ProductCard: React.FC<ProductCardProps> = ({ image, productName, price, sku, productType, productSpecificValue, isSelected, onSelect }) => {
-
+const ProductCard: React.FC<ProductCardProps> = ({
+  image,
+  productName,
+  price,
+  sku,
+  productType,
+  productSpecificValue,
+  isSelected,
+  onSelect,
+}) => {
   return (
     <div
-      className={`border rounded-3xl p-4 text-gray-600 ${isSelected ? 'border-orange-400' : 'border-gray-200'}`}
+      className={`border rounded-3xl p-4 text-gray-600 ${
+        isSelected ? "border-orange-400" : "border-gray-200"
+      }`}
       onClick={onSelect}
     >
       <div className="relative">
@@ -34,23 +43,24 @@ const ProductCard: React.FC<ProductCardProps> = ({ image, productName, price, sk
           className="absolute opacity-0 w-0 h-0"
         />
         <span
-          className={`mb-2 w-6 h-6 absolute top-3 left-3 rounded-full border-2 border-white ${isSelected ? 'bg-orange-400' : ' bg-gray-600/60'}`}
+          className={`mb-2 w-6 h-6 absolute top-3 left-3 rounded-full border-2 border-white ${
+            isSelected ? "bg-orange-400" : " bg-gray-600/60"
+          }`}
         ></span>
       </div>
-      <img src={image} className="w-full h-48 object-cover rounded-xl" alt={productName} />
+      <img
+        src={image}
+        className="w-full h-48 object-cover rounded-xl"
+        alt={productName}
+      />
       <div className="flex flex-col gap-3 p-4 items-start">
         <h3 className="text-lg font-bold">{productName}</h3>
         {/* <p className="text-gray-600">{description}</p> */}
         <p className="text-xl font-semibold">{price}</p>
         <p className="text-sm text-gray-500">{sku}</p>
-        {/* <p className="text-sm text-gray-500">{renderProductSpecificValue()}</p> */}
-        {/* {productType === 'DVD' && (
-            <p className="text-sm text-gray-500">{`Size: ${productSpecificValue} MB`}</p> 
-        )} */}
-        
-
-        <p className="text-sm text-gray-500">{productTypes(productSpecificValue)[productType]}</p>
-
+        <p className="text-sm text-gray-500">
+          {productTypes(productSpecificValue)[productType]}
+        </p>
       </div>
     </div>
   );
