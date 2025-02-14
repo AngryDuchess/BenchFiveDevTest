42ff2105b12a8300bc0374cbaf90c5aad1a0a117
Mahama Hamida <mahamahamida@gmail.com>
2025-02-14 14:39:36 +0000

    refactor AddProduct component to improve state management and integrate image upload functionality

--- src/components/AddProduct.tsx
+++ src/components/AddProduct.tsx
@@ -1,90 +1,130 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import {Product,ProductType} from '../types/products'
+import Spinner from "./spinner";
 
 const AddProduct: React.FC = () => {
   const { index } = useParams<{ index?: string }>();
+  const [product, setProduct] = useState<Product>({
+    image: "",
+    price: "",
+    sku: "",
+    description: "",
+    productName: "",
+    productType: "DVD",
+    productSpecificValue: ""
+  })
+  const [submitting, setSubmitting] = useState<boolean>(false)
+  const [imageFile, setImageFile] = useState<File | null>(null)
   const navigate = useNavigate();
-  const [sku, setSku] = useState('');
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState('');
-  const [image, setImage] = useState('');
-  const [productType, setProductType] = useState<'DVD' | 'Book' | 'Furniture'>('DVD');
-  const [productSpecificValue, setProductSpecificValue] = useState('');
-  const [dimensions, setDimensions] = useState({ height: '', width: '', length: '' });
+  const [image, setImage] = useState<string | null>("");
+  const [productSpecificValue, setProductSpecificValue] = useState("");
+  const [dimensions, setDimensions] = useState({
+    height: "",
+    width: "",
+    length: "",
+  });
 
   useEffect(() => {
     if (index !== undefined) {
-      const storedProducts = JSON.parse(localStorage.getItem('products') || '[]');
-      const product = storedProducts[parseInt(index)];
-      if (product) {
-        setSku(product.sku);
-        setName(product.productName);
-        setPrice(product.price);
-        setImage(product.image);
-        setProductType(product.productType);
-        if (product.productType === 'Furniture') {
-          const [height, width, length] = product.productSpecificValue.split(' x ');
+      const storedProducts = JSON.parse(
+        localStorage.getItem("products") || "[]"
+      );
+      const existingProduct = storedProducts[parseInt(index)];
+      if (existingProduct) {
+        setProduct({
+          ...existingProduct
+        })
+        if (existingProduct.productType === "Furniture") {
+          const [height, width, length] =
+          existingProduct.productSpecificValue.split(" x ");
           setDimensions({ height, width, length });
-        } else {
-          setProductSpecificValue(product.productSpecificValue);
+        } else {  
+          setProductSpecificValue(existingProduct.productSpecificValue);
+          setProduct({...existingProduct, productSpecificValue})
         }
       }
     }
   }, [index]);
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
+  const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const file = e.target.files && e.target.files[0];
     if (file) {
-      const imageUrl = URL.createObjectURL(file);
-      setImage(imageUrl);
+        setImageFile(file)
     }
   };
 
-  const handleSave = () => {
-    const newProduct = {
-      sku,
-      productName: name,
-      price,
-      image,
-      productType,
-      productSpecificValue: productType === 'Furniture' ? `${dimensions.height} x ${dimensions.width} x ${dimensions.length}` : productSpecificValue,
-    };
-
-    const existingProducts = JSON.parse(localStorage.getItem('products') || '[]');
-    if (index !== undefined) {
-      existingProducts[parseInt(index)] = newProduct;
-    } else {
-      existingProducts.push(newProduct);
+  const handleSave = async () => {
+    try {
+      setSubmitting(true)
+      if (imageFile) {
+        const formData = new FormData();
+        formData.append("file", imageFile);
+        formData.append("upload_preset", "ml_default");
+        const res = await axios.post('https://api.cloudinary.com/v1_1/dn5ks1ljf/upload', formData)
+          if (res.data.secure_url) {
+            setProduct({...product, image: res.data.secure_url as string})
+            setImage(res.data.secure_url)
+          }
+        }
+      const newProduct = {
+        ...product,
+        productSpecificValue:
+          product.productType === "Furniture"
+            ? `${dimensions.height} x ${dimensions.width} x ${dimensions.length}`
+            : product.productSpecificValue,
+      };
+  
+      const existingProducts = JSON.parse(
+        localStorage.getItem("products") || "[]"
+      );
+      if (index !== undefined) {
+        existingProducts[parseInt(index)] = newProduct;
+      } else {
+        existingProducts.push(newProduct);
+      }
+      localStorage.setItem("products", JSON.stringify(
+        existingProducts
+      ));
+      navigate("/");
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setSubmitting(false)
     }
-    localStorage.setItem('products', JSON.stringify(existingProducts));
-
-    navigate('/');
   };
 
+  console.log(product);
   return (
     <>
-    <Link to="/">
-          <button className="text-gray-800 py-2 px-4 hover:text-blue-600">
-            ← Go back
+      <Link to="/">
+        <button className="text-gray-800 py-2 px-4 hover:text-blue-600">
+          ← Go back
+        </button>
+      </Link>
+      <header className="flex flex-col lg:flex-row gap-8 py-3 justify-between">
+      <p className="text-2xl lg:text-5xl font-semibold">
+          {index !== undefined ? "Edit Product" : "Add a Product"}
+        </p>
+        <div className="flex gap-3">
+          <button
+            type="button"
+            onClick={handleSave}
+            className="mt-4 bg-amber-600 hover:bg-amber-700 text-white py-2 px-4 rounded flex items-center gap-2"
+          >
+            {submitting && (
+              <Spinner />
+            )}
+            {index !== undefined ? "Update" : "Save"}
           </button>
-        </Link>
-      <header className="flex  gap-8 py-3 justify-between">
-        
-        <h1 className="text-3xl font-semibold">{index !== undefined ? 'Edit Product' : 'Add a Product'}</h1>
-        <div className='flex gap-3'>
-        <button
-          type="button"
-          onClick={handleSave}
-          className="mt-4 bg-amber-600 hover:bg-amber-700 text-white py-2 px-4 rounded "
-        >
-          {index !== undefined ? 'Update Product' : 'Save Product'}
-        </button>
-        <button
-          type="button"
-          className="mt-4  hover:text-red-600 text-gray-500 py-2 px-4  "
-        >
+          <button
+            type="button"
+            className="mt-4  hover:text-red-600 text-gray-500 py-2 px-4  "
+          >
             Cancel
-        </button>
+          </button>
         </div>
       </header>
       <hr />
@@ -93,31 +133,37 @@ const AddProduct: React.FC = () => {
           <label className="block text-sm font-medium text-gray-700">SKU</label>
           <input
             type="text"
-            value={sku}
-            onChange={(e) => setSku(e.target.value)}
+            value={product.sku}
+            onChange={(e) => setProduct({...product, sku: e.target.value})}
             className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
           />
         </div>
         <div>
-          <label className="block text-sm font-medium text-gray-700">Name</label>
+          <label className="block text-sm font-medium text-gray-700">
+            Name
+          </label>
           <input
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={product.productName}
+            onChange={(e) => setProduct({...product, productName: e.target.value})}
             className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
           />
         </div>
         <div>
-          <label className="block text-sm font-medium text-gray-700">Price</label>
+          <label className="block text-sm font-medium text-gray-700">
+            Price
+          </label>
           <input
             type="text"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
+            value={product.price}
+            onChange={(e) => setProduct({...product, price: e.target.value})}
             className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
           />
         </div>
         <div>
-          <label className="block text-sm font-medium text-gray-700">Product Image</label>
+          <label className="block text-sm font-medium text-gray-700">
+            Product Image
+          </label>
           <input
             type="file"
             id="image"
@@ -128,10 +174,12 @@ const AddProduct: React.FC = () => {
           />
         </div>
         <div>
-          <label className="block text-sm font-medium text-gray-700">Product Type</label>
+          <label className="block text-sm font-medium text-gray-700">
+            Product Type
+          </label>
           <select
-            value={productType}
-            onChange={(e) => setProductType(e.target.value as 'DVD' | 'Book' | 'Furniture')}
+            value={product.productType}
+            onChange={(e) => setProduct({...product, productType: e.target.value as "DVD" | "Book" | "Furniture"})}
             className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
           >
             <option value="DVD">DVD</option>
@@ -139,29 +187,37 @@ const AddProduct: React.FC = () => {
             <option value="Furniture">Furniture</option>
           </select>
         </div>
-        {productType === 'Furniture' ? (
+        {product.productType === "Furniture" ? (
           <div>
-            <label className="block text-sm font-medium text-gray-700">Dimensions (H x W x L)</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Dimensions (H x W x L)
+            </label>
             <div className="flex space-x-2">
               <input
                 type="text"
                 placeholder="Height"
                 value={dimensions.height}
-                onChange={(e) => setDimensions({ ...dimensions, height: e.target.value })}
+                onChange={(e) =>
+                  setDimensions({ ...dimensions, height: e.target.value })
+                }
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
               />
               <input
                 type="text"
                 placeholder="Width"
                 value={dimensions.width}
-                onChange={(e) => setDimensions({ ...dimensions, width: e.target.value })}
+                onChange={(e) =>
+                  setDimensions({ ...dimensions, width: e.target.value })
+                }
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
               />
               <input
                 type="text"
                 placeholder="Length"
                 value={dimensions.length}
-                onChange={(e) => setDimensions({ ...dimensions, length: e.target.value })}
+                onChange={(e) =>
+                  setDimensions({ ...dimensions, length: e.target.value })
+                }
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
               />
             </div>
@@ -169,12 +225,12 @@ const AddProduct: React.FC = () => {
         ) : (
           <div>
             <label className="block text-sm font-medium text-gray-700">
-              {productType === 'DVD' ? 'Size (MB)' : 'Weight (Kg)'}
+              {product.productType === "DVD" ? "Size (MB)" : "Weight (Kg)"}
             </label>
             <input
               type="text"
-              value={productSpecificValue}
-              onChange={(e) => setProductSpecificValue(e.target.value)}
+              value={product.productSpecificValue}
+            onChange={(e) => setProduct({...product, productSpecificValue: e.target.value})}
               className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
             />
           </div>
