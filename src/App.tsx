01e9c78f39f27e02d45fefbc6291888141ace5e8
Mahama Hamida <mahamahamida@gmail.com>
2025-02-14 01:33:04 +0000

    implement routing for product management with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductListing from './components/ProductListing';
+import AddProduct from './components/AddProduct';
 
 function App() {
-  
-
   return (
-   <ProductListing />
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductListing />} />
+        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/edit-product/:index" element={<AddProduct />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
