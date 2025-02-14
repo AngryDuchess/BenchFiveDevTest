676e46b05f83e5083fea753d4174f2e3674e9365
Mahama Hamida <mahamahamida@gmail.com>
2025-02-14 14:39:51 +0000

    add loading animation and improve product listing layout

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 // import { products } from "../misc.data";
 import { Link, useNavigate } from "react-router-dom";
+import gif from "../assets/loading.gif"
 
 export default function ProductListing() {
   const [products, setProducts] = useState<any[]>([]);
@@ -36,13 +37,13 @@ export default function ProductListing() {
   return (
     <>
       <header className="flex justify-between py-3">
-        <h1 className="text-3xl font-semibold">Product List</h1>
+        <p className="text-2xl lg:text-5xl font-semibold">Product List</p>
         <div className="flex gap-2 items-center">
           {selectedProductIndex !== null ? (
             <>
               <button
                 onClick={handleEdit}
-                className="bg-amber-600 hover:bg-amber-700 text-white py-2 px-4 rounded"
+                className="bg-amber-600 hover:bg-amber-700 text-white py-2 px-4 rounded-xl"
               >
                 Edit
               </button>
@@ -55,7 +56,7 @@ export default function ProductListing() {
             </>
           ) : (
             <Link to="/add-product">
-              <button className="bg-amber-600 hover:bg-amber-700 text-white py-2 px-4 rounded ">
+              <button className="bg-amber-600 hover:bg-amber-700 text-white py-2 px-4 rounded-xl ">
                 Add
               </button>
             </Link>
@@ -63,22 +64,29 @@ export default function ProductListing() {
         </div>
       </header>
       <hr />
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 mt-8">
-        {products.map((product, index) => (
-          <ProductCard
-            key={index}
-            image={product.image}
-            productName={product.productName}
-            description={product.description}
-            price={product.price}
-            sku={product.sku}
-            productType={product.productType}
-            productSpecificValue={product.productSpecificValue}
-            isSelected={selectedProductIndex === index}
-            onSelect={() => handleSelect(index)}
-          />
-        ))}
-      </div>
+      {products.length === 0 ? (
+        <div className="flex flex-col gap-3 mt-10 text-center font-sm justify-center items-center h-64">
+          <img src={gif} className="w-52 h-auto" alt="Waiting..." />
+        <p>You should probably start adding products now!</p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-8">
+          {products.map((product, index) => (
+            <ProductCard
+              key={index}
+              image={product.image}
+              productName={product.productName}
+              description={product.description}
+              price={product.price}
+              sku={product.sku}
+              productType={product.productType}
+              productSpecificValue={product.productSpecificValue}
+              isSelected={selectedProductIndex === index}
+              onSelect={() => handleSelect(index)}
+            />
+          ))}
+        </div>
+      )}
     </>
   );
 }
