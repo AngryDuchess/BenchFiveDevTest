4eaa1a39bebe955de1f410065f863397b43566e0
Mahama Hamida <mahamahamida@gmail.com>
2025-02-14 14:55:38 +0000

    wrap application in ProductProvider for context management

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductListing from './components/ProductListing';
 import AddProduct from './components/AddProduct';
+import { ProductProvider } from './contexts/ProductConetxt';
 
 function App() {
   return (
     <Router>
+      <ProductProvider>
       <Routes>
         <Route path="/" element={<ProductListing />} />
         <Route path="/add-product" element={<AddProduct />} />
         <Route path="/edit-product/:index" element={<AddProduct />} />
       </Routes>
+      </ProductProvider>
     </Router>
   );
 }
