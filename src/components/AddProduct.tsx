ee273546671257a080efb0a45245344e6e3c05ac
Mahama Hamida <mahamahamida@gmail.com>
2025-02-15 08:08:06 +0000

    add validation and error handling for product submission in AddProduct component

--- src/components/AddProduct.tsx
+++ src/components/AddProduct.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import {Product} from '../types/products'
-import Spinner from "./spinner";
+import { Product } from "../types/products";
+import {Spinner, WarningIcon} from "./spinner";
 
 const AddProduct: React.FC = () => {
   const { index } = useParams<{ index?: string }>();
@@ -13,10 +13,10 @@ const AddProduct: React.FC = () => {
     description: "",
     productName: "",
     productType: "DVD",
-    productSpecificValue: ""
-  })
-  const [submitting, setSubmitting] = useState<boolean>(false)
-  const [imageFile, setImageFile] = useState<File | null>(null)
+    productSpecificValue: "",
+  });
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [imageFile, setImageFile] = useState<File | null>(null);
   const navigate = useNavigate();
   const [productSpecificValue, setProductSpecificValue] = useState("");
   const [dimensions, setDimensions] = useState({
@@ -24,6 +24,13 @@ const AddProduct: React.FC = () => {
     width: "",
     length: "",
   });
+  const [isProductNameMissing, setIsProductNameMissing] = useState(false);
+  const [isProductPriceMissing, setIsProductPriceMissing] = useState(false);
+  const [isProductSpecsMissing, setIsProductSpecsMissing] = useState(false);
+  const [isProductImageMissing, setIsProductImageMissing] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+
 
   useEffect(() => {
     if (index !== undefined) {
@@ -33,15 +40,15 @@ const AddProduct: React.FC = () => {
       const existingProduct = storedProducts[parseInt(index)];
       if (existingProduct) {
         setProduct({
-          ...existingProduct
-        })
+          ...existingProduct,
+        });
         if (existingProduct.productType === "Furniture") {
           const [height, width, length] =
-          existingProduct.productSpecificValue.split(" x ");
+            existingProduct.productSpecificValue.split(" x ");
           setDimensions({ height, width, length });
-        } else {  
+        } else {
           setProductSpecificValue(existingProduct.productSpecificValue);
-          setProduct({...existingProduct, productSpecificValue})
+          setProduct({ ...existingProduct, productSpecificValue });
         }
       }
     }
@@ -51,22 +58,42 @@ const AddProduct: React.FC = () => {
     e.preventDefault();
     const file = e.target.files && e.target.files[0];
     if (file) {
-        setImageFile(file)
+      setImageFile(file);
     }
   };
 
   const handleSave = async () => {
+    if (
+        !product.sku ||
+        !product.productName ||
+        !product.price ||
+        !product.image ||
+        (product.productType === "Furniture" &&
+          (!dimensions.height || !dimensions.width || !dimensions.length)) ||
+        (product.productType !== "Furniture" && !product.productSpecificValue)
+      ) {
+        setErrorMessage("Please, submit required data");
+        setIsProductNameMissing(!product.productName);   
+        setIsProductPriceMissing(!product.price);
+        setIsProductSpecsMissing(!product.productSpecificValue);
+        setIsProductImageMissing(!product.image)
+        setSubmitting(false);
+        return;
+      }
     try {
-      setSubmitting(true)
+      setSubmitting(true);
       if (imageFile) {
         const formData = new FormData();
         formData.append("file", imageFile);
         formData.append("upload_preset", "ml_default");
-        const res = await axios.post('https://api.cloudinary.com/v1_1/dn5ks1ljf/upload', formData)
-          if (res.data.secure_url) {
-            setProduct({...product, image: res.data.secure_url as string})
-          }
+        const res = await axios.post(
+          "https://api.cloudinary.com/v1_1/dn5ks1ljf/upload",
+          formData
+        );
+        if (res.data.secure_url) {
+          setProduct({ ...product, image: res.data.secure_url as string });
         }
+      }
       const newProduct = {
         ...product,
         productSpecificValue:
@@ -74,7 +101,7 @@ const AddProduct: React.FC = () => {
             ? `${dimensions.height} x ${dimensions.width} x ${dimensions.length}`
             : product.productSpecificValue,
       };
-  
+
       const existingProducts = JSON.parse(
         localStorage.getItem("products") || "[]"
       );
@@ -83,18 +110,15 @@ const AddProduct: React.FC = () => {
       } else {
         existingProducts.push(newProduct);
       }
-      localStorage.setItem("products", JSON.stringify(
-        existingProducts
-      ));
+      localStorage.setItem("products", JSON.stringify(existingProducts));
       navigate("/");
     } catch (e) {
-      console.error(e)
+      console.error(e);
     } finally {
-      setSubmitting(false)
+      setSubmitting(false);
     }
   };
 
-  console.log(product);
   return (
     <>
       <Link to="/">
@@ -103,7 +127,7 @@ const AddProduct: React.FC = () => {
         </button>
       </Link>
       <header className="flex flex-col lg:flex-row gap-8 py-3 justify-between">
-      <p className="text-2xl lg:text-5xl font-semibold">
+        <p className="text-2xl lg:text-5xl font-semibold">
           {index !== undefined ? "Edit Product" : "Add a Product"}
         </p>
         <div className="flex gap-3">
@@ -112,9 +136,7 @@ const AddProduct: React.FC = () => {
             onClick={handleSave}
             className="mt-4 bg-amber-600 hover:bg-amber-700 text-white py-2 px-4 rounded flex items-center gap-2"
           >
-            {submitting && (
-              <Spinner />
-            )}
+            {submitting && <Spinner />}
             {index !== undefined ? "Update" : "Save"}
           </button>
           <button
@@ -126,50 +148,83 @@ const AddProduct: React.FC = () => {
         </div>
       </header>
       <hr />
+      {errorMessage && (
+        <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
+      )}
       <form className="space-y-4 max-w-sm mt-5">
         <div>
           <label className="block text-sm font-medium text-gray-700">SKU</label>
           <input
             type="text"
             value={product.sku}
-            onChange={(e) => setProduct({...product, sku: e.target.value})}
+            onChange={(e) => setProduct({ ...product, sku: e.target.value })}
             className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
           />
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-700">
-            Name
+            Product name
           </label>
           <input
             type="text"
             value={product.productName}
-            onChange={(e) => setProduct({...product, productName: e.target.value})}
+            required
+            onChange={(e) =>
+              setProduct({ ...product, productName: e.target.value })
+            }
             className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
           />
+          {isProductNameMissing && (
+            <span className="flex gap-1 items-center mt-3">
+                <WarningIcon />
+            <p className="text-red-500 text-xs">
+              Please provide product name
+            </p>
+            </span>
+          )}
+          
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-700">
-            Price
+            Product price
           </label>
           <input
             type="text"
             value={product.price}
-            onChange={(e) => setProduct({...product, price: e.target.value})}
+            required
+            onChange={(e) => setProduct({ ...product, price: e.target.value })}
             className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
           />
+          {isProductPriceMissing && (
+            <span className="flex gap-1 items-center mt-3">
+                <WarningIcon />
+            <p className="text-red-500 text-xs">
+              Please provide a price
+            </p>
+            </span>
+          )}
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-700">
-            Product Image
+            Upload product image
           </label>
           <input
             type="file"
             id="image"
             name="image"
             accept="image/png, image/jpeg"
+            required
             onChange={handleImageChange}
             className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
           />
+          {isProductImageMissing && (
+            <span className="flex gap-1 items-center mt-3">
+                <WarningIcon />
+            <p className="text-red-500 text-xs">
+              Please provide an image
+            </p>
+            </span>
+          )}
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-700">
@@ -177,7 +232,12 @@ const AddProduct: React.FC = () => {
           </label>
           <select
             value={product.productType}
-            onChange={(e) => setProduct({...product, productType: e.target.value as "DVD" | "Book" | "Furniture"})}
+            onChange={(e) =>
+              setProduct({
+                ...product,
+                productType: e.target.value as "DVD" | "Book" | "Furniture",
+              })
+            }
             className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
           >
             <option value="DVD">DVD</option>
@@ -188,49 +248,74 @@ const AddProduct: React.FC = () => {
         {product.productType === "Furniture" ? (
           <div>
             <label className="block text-sm font-medium text-gray-700">
-              Dimensions (H x W x L)
+              Please, provide dimensions
             </label>
             <div className="flex space-x-2">
               <input
-                type="text"
+                type="number"
                 placeholder="Height"
                 value={dimensions.height}
+                required
                 onChange={(e) =>
                   setDimensions({ ...dimensions, height: e.target.value })
                 }
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
               />
               <input
-                type="text"
+                type="number"
                 placeholder="Width"
                 value={dimensions.width}
+                required
                 onChange={(e) =>
                   setDimensions({ ...dimensions, width: e.target.value })
                 }
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
               />
               <input
-                type="text"
+                type="number"
                 placeholder="Length"
                 value={dimensions.length}
+                required
                 onChange={(e) =>
                   setDimensions({ ...dimensions, length: e.target.value })
                 }
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
               />
             </div>
+            {isProductSpecsMissing && (
+            <span className="flex gap-1 items-center mt-3">
+                <WarningIcon />
+            <p className="text-red-500 text-xs">
+              Please provide product specifications
+            </p>
+            </span>
+          )}
           </div>
+          
         ) : (
           <div>
             <label className="block text-sm font-medium text-gray-700">
-              {product.productType === "DVD" ? "Size (MB)" : "Weight (Kg)"}
+              {product.productType === "DVD"
+                ? "Please, provide size size (MB)"
+                : "Please, provide size weight (Kg)"}
             </label>
             <input
-              type="text"
+              type="number"
               value={product.productSpecificValue}
-            onChange={(e) => setProduct({...product, productSpecificValue: e.target.value})}
+              required
+              onChange={(e) =>
+                setProduct({ ...product, productSpecificValue: e.target.value })
+              }
               className="mt-1 block w-full h-11 border border-gray-300 rounded-md shadow-sm"
             />
+            {isProductSpecsMissing && (
+            <span className="flex gap-1 items-center mt-3">
+                <WarningIcon />
+            <p className="text-red-500 text-xs">
+              Please provide product specifications
+            </p>
+            </span>
+          )}
           </div>
         )}
       </form>
