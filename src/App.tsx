cb5125073a047b574081e3e6ddd2f3f6a7d135ef
Mahama Hamida <mahamahamida@gmail.com>
2025-02-15 10:52:03 +0000

    update routing in App component to include ProductDetails and change AddProduct path to /new-product

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductListing from './components/ProductListing';
 import AddProduct from './components/AddProduct';
 import { ProductProvider } from './contexts/ProductConetxt';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <ProductProvider>
       <Routes>
         <Route path="/" element={<ProductListing />} />
-        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/new-product" element={<AddProduct />} />
         <Route path="/edit-product/:index" element={<AddProduct />} />
+        <Route path="/product-details/:sku/:image" element={<ProductDetails />} />
       </Routes>
       </ProductProvider>
     </Router>
